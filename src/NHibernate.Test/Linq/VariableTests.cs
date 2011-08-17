10c312dc8cf74482babe29b0c04a7b8e91067bea
Patrick Earl <patearl@patearl.net>
2011-08-16 23:04:44 -0600

    Failing test for linq variable caching.

---
+++ src/NHibernate.Test/Linq/VariableTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace NHibernate.Test.Linq
+{
+	[TestFixture]
+	public class VariableTests : LinqTestCase
+	{
+		[Test]
+		[Ignore]
+		public void ReusingSameVariable()
+		{
+			int foo = 3;
+			Assert.AreEqual(3, db.Customers.Select(c => new { c.ContactName, foo }).First().foo);
+			foo = 5;
+			Assert.AreEqual(5, db.Customers.Select(c => new { c.ContactName, foo }).First().foo);
+		}
+	}
+}
